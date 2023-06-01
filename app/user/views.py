dc37b226ecfff1feddfaebe860ba78da1b980862
Divya Singh <singh.dvya05@gmail.com>
2023-06-01 15:20:33 +0545

    user created

---
+++ app/user/views.py
@@ -0,0 +1,11 @@
+"""
+Views for the user api
+"""
+
+from rest_framework import generics
+
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
